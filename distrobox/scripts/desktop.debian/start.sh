61c076f23135f5da3e0f5e3086e84b2ce6149128
Ethorbit <Ethorbit@protonmail.com>
2023-07-28 23:41:49 -0700

    Create start.sh

---
+++ distrobox/scripts/desktop.debian/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add files that should persist through volume storage
+/persist.sh \
+	/etc/X11 \
+	/etc/lightdm \
+	/etc/i3 \
+	/etc/polybar \
+	/etc/pulse
+
+# Start the desktop
+systemctl unmask dbus.service
+systemctl start dbus.service
+while [ -z $(pidof lightdm) ]; do 
+	lightdm
+	sleep 1
+done
