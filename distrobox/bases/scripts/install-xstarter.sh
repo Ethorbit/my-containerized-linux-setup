89a0d50f0200cae353334d1e8279fe0b34640d9c
Ethorbit <Ethorbit@protonmail.com>
2024-04-29 00:54:41 -0700

    Replace distrobox with nvidia-egl-desktop
    Instead of X nested container desktops (which don't use the GPU), I've decided to go with nvidia-egl-desktop, a full blown GPU accessible desktop accessed through the browser with WebRTC.

--- distrobox/bases/scripts/install-xstarter.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/lchsk/xstarter/ /tmp/xstarter &&\
-cd /tmp/xstarter/ &&\
-cmake . &&\
-make &&\
-make install &&\
-rm -dr /tmp/xstarter/
---
+++ generate-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cat > .env << EOL
+HOST_TIMEZONE=$(cat /etc/timezone)
+# If this IP is incorrect, replace it in the generate-env.sh script.
+HOST_LOCAL_IPV4=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+TURN_SECRET=$(openssl rand -base64 64 | tr -d '\n')
+EOL
+
+[ -f .custom.env ] && cat .custom.env >> .env || cat .custom.env.template >> .env
